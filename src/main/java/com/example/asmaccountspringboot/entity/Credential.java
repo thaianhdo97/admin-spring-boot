bcb3342ad072fa333ef86ba96d9b60863be4dc5a
thaianh97 <58674701+thaianh97@users.noreply.github.com>
2021-06-21 20:00:20 +0700

    upload project

---
+++ src/main/java/com/example/asmaccountspringboot/entity/Credential.java
@@ -0,0 +1,4 @@
+package com.example.asmaccountspringboot.entity;
+
+public class Credential {
+}
---
+++ src/test/java/com/example/asmaccountspringboot/AsmAccountSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asmaccountspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmAccountSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
